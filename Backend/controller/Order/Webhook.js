d8149bc3ed90bc60b9589ba5b4de3870997543ce
Harry1468 <harshittemkar7@gmail.com>
2024-09-21 12:53:45 +0530

    webhooks

--- Backend/controller/Order/Webhook.js
+++ Backend/controller/Order/Webhook.js
@@ -2,8 +2,7 @@ const stripe = require("../../config/stripe");
 const CartproductModel = require("../../Model/cartProduct");
 const orderModel = require("../../Model/OrderProductModel");
 
-const endpointSecrete = process.env.STRIPE_ENDP
-OINT_WEBHOOK_SECRET_KEY;
+const endpointSecrete = process.env.STRIPE_ENDPOINT_WEBHOOK_SECRET_KEY;
 
 const getLineItems = async (lineItems) => {
   console.log(lineItems, "lineItems")
